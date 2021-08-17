eae3c3b6fb7b0be0a4f4ec1f70e21508dc5aafe8
amirchev <alexmirchev@hotmail.com>
2021-08-17 11:54:45 -0700

    Revert "merge local and remote heads"
    This reverts commit 08fda781924e40f93faad0f96abf37e0f580945e.

--- i18n.js
+++ i18n.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function() {
     $.i18n().load( {
-        en: 'libs/i18n/en.json',
-        es: 'libs/i18n/es.json'
+        en: 'en.json',
+        es: 'es.json'
     } )
     .done( function() { $('html').i18n(); });
 });
