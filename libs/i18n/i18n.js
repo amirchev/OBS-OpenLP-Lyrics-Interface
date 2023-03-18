863ea2bff24c8521c0e97a39b0de058d08111443
Honkey57 <51721640+Honkey57@users.noreply.github.com>
2023-03-18 11:42:10 +0100

    French translation

--- libs/i18n/i18n.js
+++ libs/i18n/i18n.js
@@ -1,7 +1,8 @@
 jQuery(document).ready(function() {
     $.i18n().load( {
-        en: 'libs/i18n/en.json',
-        es: 'libs/i18n/es.json'
+	en: 'libs/i18n/en.json',
+	es: 'libs/i18n/es.json',
+	fr: 'libs/i18n/fr.json'
     } )
     .done( function() { $('html').i18n(); });
 });
