a46cdb3ee8d8bb31b1dcf125b0a9fc2cdedadd1b
Honkey57 <51721640+Honkey57@users.noreply.github.com>
2023-04-09 19:35:33 +0200

    Add files via upload

--- libs/i18n/i18n.js
+++ libs/i18n/i18n.js
@@ -1,5 +1,7 @@
 jQuery(document).ready(function() {
     $.i18n().load( {
+        en: 'libs/i18n/en.json',
+        es: 'libs/i18n/es.json',
 	fr: 'libs/i18n/fr.json'
     } )
     .done( function() { $('html').i18n(); });
